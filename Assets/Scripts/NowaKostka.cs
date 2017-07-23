9b4e349548a4d98ca6d7b4b7cb9eab80c2d2f665
Siwy <szuminski.p@gmail.com>
2017-07-23 21:32:31 +0200

    Dodałem Nową kostkę i scene o nazwie Scena z Kostka, po to żeby sprawdzic, czy mozesz to u Siebie odczytać - Dodaje wszystkie zmiany jakie zaszły edytujac to w Unity i Visual Studio

---
+++ Assets/Scripts/NowaKostka.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NowaKostka : ItemPickUp {
+    
+	//Trzeba sprawdzic czy pojawi Ci sie kostka w scenie Gry
+    //wysyłam scenę z nowym obiektem i Kod w C Sharpie.
+    // po zaakaceptowaniu daj mi znać.
+
+	void Update () {
+        transform.Rotate(15f * Time.deltaTime, 15f * Time.deltaTime, 15f * Time.deltaTime);
+	}
+    public override void Interact()
+    {
+        print("Interakcja wywolana z Klasy Nowa Kostka, ktora jest Itemem");
+    }
+}
